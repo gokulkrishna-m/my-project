5090145e645e2b6d33a20d734413232eba4a5bb5
gokulkrishna-m <gokulkrishnamanikumar@gmail.com>
2021-02-27 20:33:25 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from '../app/header/header.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
